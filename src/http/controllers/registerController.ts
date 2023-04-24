11024b1fcf64774e509000c78b3d86164c3fa238
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-04-23 22:41:26 -0300

    chore: iniciando controller para registro de usuário

--- src/http/controllers/registerController.ts
+++ src/http/controllers/registerController.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
+import { hash } from 'bcryptjs';
 import { z } from 'zod';
 
 import { prisma } from '@/lib/prisma';
@@ -8,17 +9,19 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
-  })
+  });
 
-  const { name, email, password } = registerBodySchema.parse(request.body)
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  const password_hash = await hash(password, 6);
 
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash
     }
-  })
+  });
 
-  return reply.status(201).send()
+  return reply.status(201).send();
 }
