7bd51cfc029bf38776b2e7eaf0ae76852506d69b
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-04-24 04:48:23 -0300

    chore: lidando com erros do useCase de registerUser

--- src/http/controllers/registerController.ts
+++ src/http/controllers/registerController.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 
 import { RegisterUseCase } from '@/use-cases/registerUseCase';
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -23,7 +24,11 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
       password
     });
   } catch (error) {
-    return reply.status(409).send();
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({message: error.message});
+    }
+
+    return reply.status(500).send(); // ## TO-DO -> fix
   }
 
   return reply.status(201).send();
