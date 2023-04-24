adbbe97f7add93350a4564cae8f72acc116bd8d7
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-04-23 23:04:34 -0300

    chore: controller registro de usuário

--- src/http/controllers/registerController.ts
+++ src/http/controllers/registerController.ts
@@ -15,6 +15,16 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
 
   const password_hash = await hash(password, 6);
 
+  const userWithSameEmail = await prisma.user.findFirst({
+    where: {
+      email,
+    }
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
+
   await prisma.user.create({
     data: {
       name,
