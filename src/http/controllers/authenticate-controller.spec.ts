5045ef66485c5c1e5e8c0cbd5ddc5e223c81ea14
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-05-17 16:54:30 -0300

    chore: adicionando teste authenticate-controller e2e

--- src/http/controllers/authenticate-controller.spec.ts
+++ src/http/controllers/authenticate-controller.spec.ts
@@ -1,3 +1,33 @@
-import { test } from 'vitest';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-test('ok', () => {});
+import { app } from '@/app';
+
+describe('authenticate e2e', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to authenticate', async () => {
+
+    await request(app.server).post('/users').send({
+      name: 'Jonh Doe',
+      email: 'jonhdoe@email.com',
+      password: '123456',
+    });
+
+    const response = await request(app.server).post('/sessions').send({
+        email: 'jonhdoe@email.com',
+        password: '123456',
+      });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
